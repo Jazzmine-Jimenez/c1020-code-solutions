a739052f7f9efbdace754b140a69f69e4d1e244c
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-10-13 10:45:53 -0700

    Completed javascript-data-model

---
+++ javascript-data-model/main.js
@@ -0,0 +1,45 @@
+/* exported orderHistory */
+
+var orderHistory = [
+  {
+    name: "JavaScript for impatient programmers",
+    type: "book",
+    by: "Rauschmayer, Dr. Axel",
+    price: 31.55,
+    deliveredDate: 08082020,
+    returnByDate: 09072020
+  },
+  {
+    name: "The Timeless Ways of Building",
+    type: "book",
+    by: "Alexander, Christopher",
+    price: 41.33,
+    deliveredDate: 07202020,
+    returnByDate: 0819202
+  },
+  {
+    name: "Gamecube Controller Adapter",
+    type: ["wii U", "PC"],
+    by: "Super Smash Bros Switch",
+    price: 15.98,
+    deliveredDate: 07072020,
+    returnByDate: 08052020
+  },
+  {
+    name: "Gamecube Controller",
+    type: "Super Smash Bros. Edition",
+    by: "Nintendo Switch",
+    price: 94.95,
+    deliveredDate: 07052020,
+    returnByDate: 08042020
+  },
+  {
+    name: "The Art of Sql",
+    type: "book",
+    by: "Stephane Faroult",
+    price: 33.99,
+    deliveredDate: 07052020,
+    returnByDate: 08042020
+  }
+
+];
