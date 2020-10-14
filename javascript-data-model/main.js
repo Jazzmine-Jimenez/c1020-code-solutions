3b25cc11efd3def0c2278daaa7392443d55f4415
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-10-13 20:54:40 -0700

    Updated - using objects within array

--- javascript-data-model/main.js
+++ javascript-data-model/main.js
@@ -2,60 +2,54 @@
 
 var orderHistory = [
   {
-    var order0 = [
-    name: "JavaScript for impatient programmers",
-    type: "book",
-    by: "Rauschmayer, Dr. Axel",
-    price: 31.55,
     deliveredDate: 08082020,
     returnByDate: 09072020,
-    orderNumber: 11439416898772232
-    ]
+    orderNumber: 11439416898772232,
+    item: [{
+      name: "JavaScript for impatient programmers",
+      by: "Rauschmayer, Dr. Axel",
+      type: "book",
+      price: 31.55
+    }]
   },
   {
-    var order1 = [
-    name: "The Timeless Ways of Building",
-    type: "book",
-    by: "Alexander, Christopher",
-    price: 41.33,
     deliveredDate: 07202020,
     returnByDate: 08192020,
-    orderNumber: 11399842681280257
-    ]
+    orderNumber: 11399842681280257,
+    item: [{
+      name: "The Timeless Ways of Building",
+      by: "Alexander, Christopher",
+      type: "book",
+      price: 41.33
+    }]
   },
   {
-    var [
-    name: "Gamecube Controller Adapter",
-    type: "wii U or PC",
-    by: "Super Smash Bros Switch",
-    price: 15.98,
     deliveredDate: 07072020,
     returnByDate: 08052020,
     orderNumber: 11428755579059409
-    ]
+    item: [{
+      name: "Gamecube Controller Adapter",
+      by: "Super Smash Bros Switch",
+      type: "wii U or PC",
+      price: 15.98
+    }]
   },
-
   {
-    [
-    {
-    name: "Gamecube Controller",
-    type: "Super Smash Bros. Edition",
-    by: "Nintendo Switch",
-    price: 94.95,
     deliveredDate: 07052020,
     returnByDate: 08042020,
     orderNumber: 11328831772648248
-    },
-    {
-    name: "The Art of Sql",
-    type: "book",
-    by: "Stephane Faroult",
-    price: 33.99,
-    deliveredDate: 07052020,
-    returnByDate: 08042020,
-    orderNumber: 11328831772648248
-    }
-    ]
-
-  },
+    item: [
+      {
+        name: "The Art of Sql",
+        by: "Stephane Faroult",
+        type: "book",
+        price: 33.99
+      },
+      {
+        name: "Gamecube Controller",
+        by: "Nintendo Switch",
+        type: "Super Smash Bros. Edition",
+        price: 94.95
+      }
+    ]}
 ];
