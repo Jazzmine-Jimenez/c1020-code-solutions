e10b73a57074b1c97d8e86bf3e855da1c2a01069
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-10-16 18:33:25 -0700

    In progress String-manipulation-1

---
+++ string-manipulation-1/src/capitalize.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+function capitalize(word){
+  debugger;
+  var newWord = "";
+  newWord = word.toLowerCase();
+  firstLetter = word[0].toUpperCase();
+  newWord[0] = firstLetter;
+  return newWord;
+
+}
