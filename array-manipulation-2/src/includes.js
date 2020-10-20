27663e817800d8d19339f0881c01a0cff52f9dac
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-10-20 10:24:23 -0700

    completed inludes

--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,14 +1,9 @@
 /* eslint-disable no-unused-vars */
 function includes(array, value){
-  debugger;
-  for (var i = 0; i < array.length; i++){
+  for (var i = 0; i <= array.length; i++){
     var statement = false;
     if (array[i] === value){
       var statement = true;
-      break;
-    }
-    else if(array[i] == null){
-      var statement = null;
       break;
     }
     else continue;
--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,1 +1,9 @@
 /* eslint-disable no-unused-vars */
+function takeRight(array, count){
+  debugger;
+  var newArray = [];
+  for (var i = array.length - 1; i > count; i--){
+       newArray.push(array[i]);
+    }
+  return newArray;
+}
