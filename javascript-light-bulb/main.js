8225cd92a0c59224f68d1a8cf46baf9807da96ca
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-10-20 13:19:37 -0700

    Completed javascript-light-bulb

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,8 +1,20 @@
-$circleButton = document.querySelector("button.circle")
+$circleButton = document.querySelector(".circle", )
+$background = document.querySelector(".container")
 
 $circleButton.addEventListener("click", change);
 
 function change(event){
-  var status;
-  "" = button.textContent
+  var statusOfButton = $circleButton.className;
+  var statusOfBackground = $background.className;
+
+  if (statusOfButton === "circle on"){
+    statusOfButton = "circle off";
+    statusOfBackground = "container off";
+  }
+  else{
+    statusOfButton = "circle on";
+    statusOfBackground = "container on";
+  }
+$circleButton.className = statusOfButton;
+$background.className = statusOfBackground;
 }
