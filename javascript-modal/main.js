2c75c9cd4de2c818975181f75599cdd5028809e9
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-10-20 19:44:47 -0700

    need to fix transparent background

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,13 +1,17 @@
 
 var $popUp = document.querySelector(".pop-up-container");
+var $background = document.querySelector(".container")
 
 var $modalButton = document.querySelector(".modal-button")
+
 $modalButton.addEventListener("click", function(event){
   $popUp.className = "pop-up-container opened";
+  $background.className = "container open";
   });
 
 
-// $surveyButton = document.querySelector(".survey-button")
-// $surveyButton.addEventListener("click", function(event){
-
-// })
+var $surveyButton = document.querySelector(".survey-button")
+$surveyButton.addEventListener("click", function(event){
+  $popUp.className = "pop-up-container";
+  $background.className = "container";
+});
