b04e8acf8b2c344eff11850357b90461293496bc
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-10-20 18:48:22 -0700

    javascript-modal in progress

---
+++ javascript-modal/main.js
@@ -0,0 +1,13 @@
+
+var $popUp = document.querySelector(".pop-up-container");
+
+var $modalButton = document.querySelector(".modal-button")
+$modalButton.addEventListener("click", function(event){
+  $popUp.className = "pop-up-container opened";
+  });
+
+
+// $surveyButton = document.querySelector(".survey-button")
+// $surveyButton.addEventListener("click", function(event){
+
+// })
