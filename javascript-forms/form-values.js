c882227d272bc2356ef2070be962c91e707e9098
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-10-21 11:46:30 -0700

    fixed variables in form-values.js

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -2,9 +2,9 @@ $contactForm = document.forms[0]
 $contactForm.addEventListener("submit", function(event){
   event.preventDefault();
   var formValues = {
-    $name: $contactForm.elements.name.value,
-    $email: $contactForm.elements.email.value,
-    $message: $contactForm.elements.message.value
+    name: $contactForm.elements.name.value,
+    email: $contactForm.elements.email.value,
+    message: $contactForm.elements.message.value
   }
   console.log("Object:", formValues);
   $contactForm.reset();
