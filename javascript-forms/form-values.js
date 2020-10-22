aaca7e21cd13e0d05a3e5423f207380386d93f05
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-10-22 11:56:04 -0700

    fixed properties

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -1,11 +1,11 @@
-$contactForm = document.forms[0]
-$contactForm.addEventListener("submit", function(event){
+var $contactForm = document.forms[0];
+$contactForm.addEventListener('submit', function (event) {
   event.preventDefault();
   var formValues = {
     name: $contactForm.elements.name.value,
     email: $contactForm.elements.email.value,
     message: $contactForm.elements.message.value
-  }
-  console.log("Object:", formValues);
+  };
+  console.log('Object:', formValues);
   $contactForm.reset();
 });
