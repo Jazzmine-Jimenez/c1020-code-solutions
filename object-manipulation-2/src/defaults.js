194f129d2653ce346966ca48d9c90394c4d6bd81
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-11-01 22:02:36 -0800

    last test for defualt function

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,10 +1,27 @@
 /* eslint-disable no-unused-vars */
 function defaults(target, source) {
+  debugger;
   for (var targetKey in target) {
-    for (var sourceKey in source) {
-      if (targetKey !== sourceKey) {
-        target[sourceKey] = source[sourceKey];
+
+    if (targetKey === undefined) {
+      for (var sourceKey in source) {
+        var value = source[sourceKey];
+        target[sourceKey] = value;
       }
+    }
+    for (sourceKey in source) {
+      if (targetKey !== sourceKey) {
+        value = source[sourceKey];
+        target[sourceKey] = value;
+      }
+      if (targetKey === sourceKey) {
+        console.log('inside targetKey for statement');
+        break;
+      }
+
     }
   }
 }
+
+// last test need to be able to keep target key with original values. goes through targetkey for loop but does not more in
+// properties in the sourceKey for loop
