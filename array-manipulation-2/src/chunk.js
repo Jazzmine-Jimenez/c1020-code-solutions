6e9b544c967a9db388bf1c78d318b9ad7660ba08
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-11-04 12:18:48 -0800

    chunk in progress

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,13 +1,31 @@
 /* eslint-disable no-unused-vars */
+// function chunk(array, size) {
+//   var newArray = [];
+//   for (var i = 0; i < array.length; i++) {
+//     var numOfCycles = 0;
+//     // while (numOfCycles < 0) {
+//     //   newArray[numOfCycles] = array[i];
+//     //   i++;
+//     // }
+//     // numOfCycles++;
+//   }
+//   return newArray;
+// }
+
 function chunk(array, size) {
   var newArray = [];
+  var rotation = 0;
   for (var i = 0; i < array.length; i++) {
-    var numOfCycles = 0;
-    // while (numOfCycles < 0) {
-    //   newArray[numOfCycles] = array[i];
-    //   i++;
-    // }
-    // numOfCycles++;
+    var tempArray = [];
+    if (size > rotation) {
+      tempArray.push(array[i]);
+      rotation++;
+    }
+    if (rotation === 2) {
+      rotation = 0;
+      continue;
+    }
+    newArray.push(tempArray);
   }
   return newArray;
 }
