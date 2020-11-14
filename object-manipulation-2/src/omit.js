a33aab5392de2662821750d68bb8b6a4171b8fc2
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-11-14 13:53:41 -0800

    updated omit and pick functions

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -4,7 +4,6 @@ function omit(source, keys) {
   for (var key in source) {
     for (var i = 0; i < keys.length; i++) {
       if (keys[i] === key) break;
-      else continue;
     }
     if (keys[i] !== key) {
       newObject[key] = source[key];
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -3,8 +3,7 @@ function pick(source, keys) {
   var newObj = {};
   for (var key in source) {
     for (var i = 0; i < keys.length; i++) {
-      if (keys[i] !== key) continue;
-      else {
+      if (keys[i] === key) {
         if (source[keys[i]] !== undefined) {
           newObj[keys[i]] = source[keys[i]];
           break;
