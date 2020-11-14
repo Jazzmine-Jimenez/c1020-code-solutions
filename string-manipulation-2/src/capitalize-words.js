e2db02d4515517afb3765f0c9768d8c5469ef900
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-11-14 14:35:22 -0800

    Completed truncate, capitalizeWord, ransomCase functions

---
+++ string-manipulation-2/src/capitalize-words.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+function capitalizeWord(word) {
+  var endOfWord = word.substring(1, word.length);
+  endOfWord = endOfWord.toLowerCase();
+  var firstLetter = word[0].toUpperCase();
+  var newWord = firstLetter + endOfWord;
+
+  if (word.toLowerCase() === 'javascript') {
+    return 'JavaScript';
+  }
+  return newWord;
+
+}
