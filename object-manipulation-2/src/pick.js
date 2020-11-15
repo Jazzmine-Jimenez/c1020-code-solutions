303a6e92440af762b60c6cae72915345f9b78e78
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-11-15 12:07:08 -0800

    updated pick function

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,14 +1,9 @@
 /* eslint-disable no-unused-vars */
 function pick(source, keys) {
   var newObj = {};
-  for (var key in source) {
-    for (var i = 0; i < keys.length; i++) {
-      if (keys[i] === key) {
-        if (source[keys[i]] !== undefined) {
-          newObj[keys[i]] = source[keys[i]];
-          break;
-        }
-      }
+  for (var i = 0; i < keys.length; i++) {
+    if (source[keys[i]] !== undefined) {
+      newObj[keys[i]] = source[keys[i]];
     }
   }
   return newObj;
