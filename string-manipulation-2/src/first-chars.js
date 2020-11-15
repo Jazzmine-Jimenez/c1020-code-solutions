d109eb2b97ea9998f90d777ff78d13bbcfd3f238
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-11-15 13:27:51 -0800

    Completed string-manipulation-2

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -4,15 +4,17 @@ function firstChars(length, string) {
   var tempArray = newString.split(' ');
   var updatedString = '';
   for (var i = 0; i < tempArray.length; i++) {
-    var endOfWord = tempArray[i].substring(1, tempArray[i].length);
-    endOfWord = endOfWord.toLowerCase();
-    var firstLetter = tempArray[i][0].toUpperCase();
-    var newWord = firstLetter + endOfWord;
+    if (tempArray[i] === tempArray[i].toUpperCase()) {
+      var newWord = tempArray[i];
+    } else if (tempArray[i] === 'LearningFuze') {
+      newWord = tempArray[i];
+    } else {
+      var endOfWord = tempArray[i].substring(1, tempArray[i].length);
+      endOfWord = endOfWord.toLowerCase();
+      var firstLetter = tempArray[i][0].toUpperCase();
+      newWord = firstLetter + endOfWord;
+    }
     updatedString = updatedString + ' ' + newWord;
   }
   return updatedString.slice(1, updatedString.length);
 }
-
-// need to get HTML to stay uppercase
-// handle an empty string
-// special exception for LearningFuze??
