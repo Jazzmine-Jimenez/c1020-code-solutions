cff6ad8bf9742a946591a348c3b17f36a35427b6
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-11-19 14:33:51 -0800

    Completed isAnagram function

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,13 @@
 /* eslint-disable no-unused-vars */
+function isAnagram(firstString, secondString) {
+  var newFirst = firstString.replaceAll(' ', '');
+  var newSecond = secondString.replaceAll(' ', '');
+
+  if (newFirst.length !== newSecond.length) return false;
+
+  for (var i = 0; i < newFirst.length; i++) {
+    var statement = newFirst.includes(newSecond[i]);
+    if (statement === false) return statement;
+  }
+  return statement;
+}
