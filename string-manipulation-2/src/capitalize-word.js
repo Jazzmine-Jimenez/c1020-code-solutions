32d967127983a02ed9d2a0fb9aa8145e99af110e
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-11-19 10:49:34 -0800

    Updated capitalize-word and first-chars functions

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-unused-vars */
 function capitalizeWord(word) {
+  if (word.toLowerCase() === 'javascript') {
+    return 'JavaScript';
+  }
+
   var endOfWord = word.substring(1, word.length);
   endOfWord = endOfWord.toLowerCase();
   var firstLetter = word[0].toUpperCase();
   var newWord = firstLetter + endOfWord;
 
-  if (word.toLowerCase() === 'javascript') {
-    return 'JavaScript';
-  }
   return newWord;
 }
--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,20 +1,5 @@
 /* eslint-disable no-unused-vars */
 function firstChars(length, string) {
-  var newString = string.substring(0, length);
-  var tempArray = newString.split(' ');
-  var updatedString = '';
-  for (var i = 0; i < tempArray.length; i++) {
-    if (tempArray[i] === tempArray[i].toUpperCase()) {
-      var newWord = tempArray[i];
-    } else if (tempArray[i] === 'LearningFuze') {
-      newWord = tempArray[i];
-    } else {
-      var endOfWord = tempArray[i].substring(1, tempArray[i].length);
-      endOfWord = endOfWord.toLowerCase();
-      var firstLetter = tempArray[i][0].toUpperCase();
-      newWord = firstLetter + endOfWord;
-    }
-    updatedString = updatedString + ' ' + newWord;
-  }
-  return updatedString.slice(1, updatedString.length);
+  var newString = string.slice(0, length);
+  return newString;
 }
