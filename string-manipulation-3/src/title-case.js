c652a00830cf6b9e5d9badd35ca5b6a1727a7208
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-11-20 11:27:03 -0800

    Completed title-case function

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -2,14 +2,19 @@
 function titleCase(title) {
   var tempArray = title.split(' ');
   var updatedString = '';
-
+  var holder;
   for (var i = 0; i < tempArray.length; i++) {
-    var charIndex = tempArray[i].indexOf(':');
-    // console.log('tempArray[i]:', tempArray[i]);
-    if (i === 0) {
+    var semicolonIndex = tempArray[i].indexOf(':');
+    var dashIndex = tempArray[i].indexOf('-');
+    if (i === 0 && tempArray[i].toLowerCase() !== 'javascript:') {
       var endOfWord = tempArray[i].substring(1, tempArray[i].length);
       var firstLetter = tempArray[i][0].toUpperCase();
       var newWord = firstLetter + endOfWord;
+      updatedString = updatedString + ' ' + newWord;
+    } else if (tempArray[i] === holder) {
+      endOfWord = tempArray[i].substring(1, tempArray[i].length);
+      firstLetter = tempArray[i][0].toUpperCase();
+      newWord = firstLetter + endOfWord;
       updatedString = updatedString + ' ' + newWord;
     } else if (tempArray[i] === 'web') {
       updatedString = updatedString + ' Web';
@@ -17,25 +22,28 @@ function titleCase(title) {
       updatedString = updatedString + ' API';
     } else if (tempArray[i].length <= 3 && i !== 0) {
       updatedString = updatedString + ' ' + tempArray[i];
-    } else if (tempArray[i] === 'javascript' || tempArray[i] === 'Javascript' || tempArray[i] === 'JavaScript') {
+    } else if (tempArray[i].toLowerCase() === 'javascript') {
       updatedString = updatedString + ' JavaScript';
+    } else if (tempArray[i].toLowerCase() === 'javascript:') {
+      updatedString = updatedString + ' JavaScript:';
+      if (semicolonIndex !== -1) {
+        holder = tempArray[i + 1];
+      }
     } else if (tempArray[i].length > 3 && i !== 0) {
-      endOfWord = tempArray[i].substring(1, tempArray[i].length);
-      firstLetter = tempArray[i][0].toUpperCase();
-      newWord = firstLetter + endOfWord;
-      updatedString = updatedString + ' ' + newWord;
+      if (dashIndex !== -1) {
+        firstLetter = tempArray[i][0].toUpperCase();
+        var letterAfterDash = tempArray[i][dashIndex + 1].toUpperCase();
+        var firstHalf = tempArray[i].substring(1, dashIndex + 1);
+        var secondHalf = tempArray[i].substring(dashIndex + 2, tempArray.length + 1);
+        newWord = firstLetter + firstHalf + letterAfterDash + secondHalf;
+        updatedString = updatedString + ' ' + newWord;
+      } else {
+        endOfWord = tempArray[i].substring(1, tempArray[i].length);
+        firstLetter = tempArray[i][0].toUpperCase();
+        newWord = firstLetter + endOfWord;
+        updatedString = updatedString + ' ' + newWord;
+      }
     }
   }
-  console.log(updatedString);
   return updatedString.trimStart();
 }
-
-// var endOfWord = tempArray[i].substring(1, tempArray[i].length);
-// endOfWord = endOfWord.toLowerCase();
-// var firstLetter = tempArray[i][0].toUpperCase();
-// var newWord = firstLetter + endOfWord;
-// updatedString = updatedString + ' ' + newWord;
-
-// else if (charIndex !== -1) {
-//   console.log('charIndex:', charIndex);
-// }
