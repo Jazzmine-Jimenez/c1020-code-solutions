bd5c9c78a3be74029ac3f1d1672d1617f71a8c35
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-11-21 15:50:21 -0800

    difference in progress

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+function difference(first, second) {
+  var newArray = [];
+  for (var i = 0; i < first.length; i++) {
+    var statement = second.includes(first[i]);
+    if (!statement) {
+      newArray.push(first[i]);
+    }
+  }
+  console.log(newArray);
+  return newArray;
+}
