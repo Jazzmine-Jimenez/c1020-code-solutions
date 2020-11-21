8185f15948f76d91698d0880f3846b0656643ee5
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-11-21 12:14:01 -0800

    Completed zip function

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,13 @@
 /* eslint-disable no-unused-vars */
+function zip(first, second) {
+  var mainArray = [];
+  for (var i = 0; i < first.length; i++) {
+    var tempArray = [];
+    if (first[i] !== undefined && second[i] !== undefined) {
+      tempArray.push(first[i]);
+      tempArray.push(second[i]);
+      mainArray.push(tempArray);
+    }
+  }
+  return mainArray;
+}
