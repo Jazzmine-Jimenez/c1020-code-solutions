9a3866b87c265282a52f9a6b9c4ab5a1ac20b6cd
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-11-21 13:17:27 -0800

    Completed unique function

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-unused-vars */
+function unique(array) {
+  var tempArray = array;
+  for (var i = 0; i < array.length; i++) {
+    for (var n = tempArray.length - 1; n >= 0; n--) {
+      if (array[i] === tempArray[n]) {
+        if (i !== n) {
+          tempArray.splice(n, 1);
+        }
+      }
+    }
+  }
+  return tempArray;
+}
