d042587bdb963dbb14d0836e2704e00598c411e7
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-11-22 17:05:27 -0800

    resubmission

--- dom-events/buttons.js
+++ dom-events/buttons.js
@@ -1,27 +1,26 @@
-function handleClick(event){
-  console.log("button clicked");
+function handleClick(event) {
+  console.log('button clicked');
   console.log(event);
-  console.log(".click-button");
+  console.log('.click-button');
 }
 
-var $clickButton = document.querySelector(".click-button");
-$clickButton.addEventListener("click", handleClick);
+var $clickButton = document.querySelector('.click-button');
+$clickButton.addEventListener('click', handleClick);
 
-
-function handleMouseover(event){
-  console.log("button hovered");
+function handleMouseover(event) {
+  console.log('button hovered');
   console.log(event);
-  console.log(".hover-button");
+  console.log('.hover-button');
 }
 
-var $hover = document.querySelector(".hover-button");
-$hover.addEventListener("mouseover", handleMouseover)
+var $hover = document.querySelector('.hover-button');
+$hover.addEventListener('mouseover', handleMouseover);
 
-function handleDoubleClick(event){
-  console.log("button double-clicked")
+function handleDoubleClick(event) {
+  console.log('button double-clicked');
   console.log(event);
-  console.log(".double-click-button")
+  console.log('.double-click-button');
 }
 
-var $double = document.querySelector(".double-click-button");
-$double.addEventListener("dblclick", handleDoubleClick);
+var $double = document.querySelector('.double-click-button');
+$double.addEventListener('dblclick', handleDoubleClick);
