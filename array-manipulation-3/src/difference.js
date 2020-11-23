7016c4603de396074344feac5c06e94b64cfe498
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-11-23 14:46:52 -0800

    difference function in progress

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -2,11 +2,27 @@
 function difference(first, second) {
   var newArray = [];
   for (var i = 0; i < first.length; i++) {
-    var statement = second.includes(first[i]);
-    if (!statement) {
+    var index = second.indexOf(first[i]);
+    if (index === -1) {
       newArray.push(first[i]);
+    }
+    index = first.indexOf(second[i]);
+    if (index === -1) {
+      newArray.push(second[i]);
     }
   }
   console.log(newArray);
   return newArray;
 }
+
+// function difference(first, second) {
+//   var newArray = [];
+//   for (var i = 0; i < first.length; i++) {
+//     var statement = second.includes(first[i]);
+//     if (!statement) {
+//       newArray.push(first[i]);
+//     }
+//   }
+//   console.log(newArray);
+//   return newArray;
+// }
