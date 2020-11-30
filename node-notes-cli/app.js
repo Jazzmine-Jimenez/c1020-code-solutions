76aaf65c1a4621ee110ffbffb95dddfb424f042f
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-11-30 13:16:34 -0800

    begin delete

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -5,23 +5,17 @@ if (process.argv[2] === 'read') {
   fs.readFile('./data.json', 'utf8', (err, data) => {
     if (err) throw err;
     const dataObj = JSON.parse(data);
-    for (var entries in dataObj.notes) { console.log(`${entries}. ${dataObj.notes[entries]}`); }
+    for (var entries in dataObj.notes) { console.log(`${entries}: ${dataObj.notes[entries]}`); }
   });
 }
 
 if (process.argv[2] === 'create') {
   const id = data.nextId;
   const newNote = process.argv[3];
-  data.note++;
+  data.nextId++;
   data.notes[id] = newNote;
   const jsonData = JSON.stringify(data);
   fs.writeFile('./data.json', jsonData, 'utf8', err => {
     if (err) throw err;
   });
 }
-
-// fs.appendFile('./data.json', JSON.stringify(newObj), (err, data) => {
-//   if (err) throw err;
-// });
-
-// dataObj.notes[entries[process.argv[3]]];
