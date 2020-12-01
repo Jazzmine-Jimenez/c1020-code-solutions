b9cbbc015e450f802681ddb2956c69f4bffed87d
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-12-01 10:21:16 -0800

    Completed express-delete

--- express-delete/index.js
+++ express-delete/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, () => {
-  console.log('listening on port 3000');
+  process.stdout.write('listening on port 3000');
 });
 
 const grades = [
@@ -25,23 +25,11 @@ app.get('/api/grades', function (req, res) {
 });
 
 app.delete('/api/grades/:id', function (req, res) {
-  const deleteId = req.params.id;
-  console.log('deleteId:', deleteId);
+  const deleteId = Number(req.params.id);
   for (let i = 0; i < grades.length; i++) {
-    console.log('grades[i].id:', grades[i].id);
     if (grades[i].id === deleteId) {
-      console.log('in in');
+      grades.splice(i, 1);
     }
   }
+  res.sendStatus(204);
 });
-
-// app.delete('/api/grades/:id', function (req, res) {
-//   for (var i = 0; i < grades.length; i++) {
-//     // if (grades[i].id === req.params.id) {
-//     //   res.send(grades[i].id);
-//     // grades.splice(i, 1);
-//     // }
-//     console.log(grades[i]);
-//   }
-//   // res.sendStatus();
-// });
