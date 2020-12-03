57d8fc624a92b68797c73f05f8686de4cb5a2f8f
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-12-02 20:55:03 -0800

    in progress

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,9 +1,33 @@
 const letters = document.querySelectorAll('span');
 
-document.addEventListener('keydown', key => {
-  console.log(letters[0]);
-
-  if (letters[0] === event.target) {
-    console.log('clicked H');
+document.addEventListener('keydown', event => {
+  if (letters[0]) {
+    if (event.key === 'h') {
+      letters[0].className = 'clicked current-letter';
+    } else {
+      letters[0].className = 'clicked wrong-letter';
+    }
   }
+
+  // if (event.key === 'h') {
+  //   letters[i].className = 'clicked current-letter';
+  // }
+  // if (event.key === 'h') {
+  //   letters[0].className = 'clicked current-letter';
+  // } else if (event.key === 'e') {
+  //   letters[0].className = 'clicked';
+  //   letters[1].className = 'clicked current-letter';
+  // } else if (event.key === 'l') {
+  //   if (letters[2].className !== 'clicked current-letter') {
+  //     letters[1].className = 'clicked';
+  //     letters[2].className = 'clicked current-letter';
+  //   } else {
+  //     letters[2].className = 'clicked';
+  //     letters[3].className = 'clicked current-letter';
+  //   }
+  // } else if (event.key === 'o') {
+  //   letters[3].className = 'clicked';
+  //   letters[4].className = 'clicked current-letter';
+  // }
+
 });
