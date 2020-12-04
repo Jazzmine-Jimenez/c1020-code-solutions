394c94b238bcf1feb348c07eae4d43e34f6c94a1
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-12-03 17:46:12 -0800

    Completed javascript-typing-tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,46 +1,18 @@
 const letters = document.querySelectorAll('span');
+let index = 0;
 
 document.addEventListener('keydown', event => {
+  if (index >= letters.length) return;
 
-  for (let i = 0; i < letters.length; i++){
-    if (letters[i]) {
-      if ((letters[0].textContent).toLowerCase() === event.key) {
-      letters[0].className = 'clicked current-letter';
-      }else if ((letters[0].textContent).toLowerCase() !== event.key) {
-      letters[0].className = 'wrong-letter';
+  if ((letters[index].textContent).toLowerCase() === event.key) {
+    if (index !== 0) {
+      letters[index - 1].className = 'clicked';
     }
+    letters[index].className = 'current-letter';
+    letters[index + 1].className = 'clicked';
+    letters[index + 1].className = 'next-letter';
+    index++;
+  } else {
+    letters[index].className = 'wrong-letter';
   }
-
-
-  // if ((letters[0].textContent).toLowerCase() === event.key) {
-  //   letters[0].className = 'clicked current-letter';
-  // } else if ((letters[0].textContent).toLowerCase() !== event.key) {
-  //   letters[0].className = 'wrong-letter';
-  // } else if (letters[1].textContent === event.key) {
-  //   letters[1].className = 'clicked current-letter';
-  // } else if (letters[1].textContent !== event.key) {
-  //   letters[1].className = 'wrong-letter';
-  // }
-
-  // if (event.key === 'h') {
-  //   letters[i].className = 'clicked current-letter';
-  // }
-  // if (event.key === 'h') {
-  //   letters[0].className = 'clicked current-letter';
-  // } else if (event.key === 'e') {
-  //   letters[0].className = 'clicked';
-  //   letters[1].className = 'clicked current-letter';
-  // } else if (event.key === 'l') {
-  //   if (letters[2].className !== 'clicked current-letter') {
-  //     letters[1].className = 'clicked';
-  //     letters[2].className = 'clicked current-letter';
-  //   } else {
-  //     letters[2].className = 'clicked';
-  //     letters[3].className = 'clicked current-letter';
-  //   }
-  // } else if (event.key === 'o') {
-  //   letters[3].className = 'clicked';
-  //   letters[4].className = 'clicked current-letter';
-  // }
-
 });
