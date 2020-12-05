a8f0d7add1f7e1dad3c455591cf6ef7f8449196c
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-12-04 17:50:53 -0800

    start of delete route

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -99,6 +99,47 @@ app.put('/api/grades/:gradeId', (req, res) => {
 
 });
 
+app.delete('/api/grades/:gradeId', (req, res) => {
+  const deleteId = req.params.gradeId;
+  const name = req.body.name;
+  const course = req.body.course;
+  const score = Number(req.body.score);
+  const values = [deleteId, name, course, score];
+
+  console.log(values);
+
+  //   if (score < 0 || score > 100 || !Number.isInteger(score)) {
+  //     res.status(404).json(
+  //       { error: 'Must include a score as an interger between 1 - 100' });
+  //   } else if (!name) {
+  //     res.status(404).json(
+  //       { error: 'Must include a name' });
+  //   } else if (!course) {
+  //     res.status(404).json(
+  //       { error: 'Must include a course' });
+  //   }
+  //   const sql = `
+  //     update "grades"
+  //        set "name" = $2,
+  //            "course" = $3,
+  //            "score" = $4
+  //      where "gradeId" = $1
+  //  returning *
+  //     `;
+  //   db.query(sql, values)
+  //     .then(result => {
+  //       const updatedGrade = result.rows[0];
+  //       res.status(200).json(updatedGrade);
+  //     })
+  //     .catch(err => {
+  //       console.error(err);
+  //       res.status(500).json({
+  //         error: 'An unexpected error occurred.'
+  //       });
+  //     });
+
+});
+
 app.listen(3000, () => {
   process.stdout.write('listening on port 3000!');
 });
