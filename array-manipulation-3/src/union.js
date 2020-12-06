e2f6a42f50940131ecd8010a011e1a4cdea0641e
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-12-06 14:52:11 -0800

    Completed union function

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,17 @@
 /* eslint-disable no-unused-vars */
+function union(first, second) {
+  const newArray = [];
+  for (let n = 0; n < first.length; n++) {
+    if (second.includes(first[n])) {
+      newArray.push(first[n]);
+    } else {
+      newArray.push(first[n]);
+    }
+  }
+  for (let i = 0; i < second.length; i++) {
+    if (!first.includes(second[i])) {
+      newArray.push(second[i]);
+    }
+  }
+  return newArray;
+}
