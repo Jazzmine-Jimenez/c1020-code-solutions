b941981b573d09a963d683267d8e10d7a114ae34
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-12-10 16:58:37 -0800

    toggles from on to off

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 
 export default class ToggleButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { click: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      click: true
+    });
+  }
 
   render() {
-    return (
-      <div className="oval">
-        <div className="circle"></div>
-        <p className="switch">OFF</p>
-      </div>
-    );
+    if (this.state.click) {
+      return (
+        <div onClick={this.handleClick} className="container">
+          <div className="oval">
+            <div className="circle off"></div>
+          </div>
+          <p className="switch">OFF</p>
+        </div>
+      );
+    } else {
+      return (
+        <div onClick={this.handleClick} className="container">
+          <div className="oval on">
+            <div className="circle on"></div>
+          </div>
+          <p className="switch">ON</p>
+        </div>
+      );
+    }
   }
 }
