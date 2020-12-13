7724537ef00eeb2c21851bcc7508cce79895dd55
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-12-12 16:35:19 -0800

    renders drawer

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
 
 export default class Drawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isDrawerOpen: false };
+  }
+
   render() {
-    return (
-      <div className="container">
-        <div className="menu-button">
-          <i className="fas fa-bars"></i>
+    if (!this.isDrawerOpen) {
+      return (
+        <div className="container">
+          <div className="drawer">
+            <div className="row">
+              <div className="column-one-third">
+                <div className="menu-box">
+                  <div className="menu-items">
+                    <h2>Menu</h2>
+                    <h5>About</h5>
+                    <h5>Get Started</h5>
+                    <h5>Sign In</h5>
+                  </div>
+                </div>
+              </div>
+              <div className="column-two-third">
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="container">
+          <div className="menu-button">
+            <i className="fas fa-bars"></i>
+          </div>
+        </div>
+      );
+    }
   }
 }
