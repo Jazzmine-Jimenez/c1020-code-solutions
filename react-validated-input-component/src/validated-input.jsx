62f94437bc65f3b0306a9187b8c502fa0410274e
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-12-13 15:37:32 -0800

    no response displays x and error message

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -6,28 +6,56 @@ export default class Validate extends React.Component {
     this.state = { password: '' };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ password: event.target.password });
+    this.setState({ password: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('Password:', this.state.password);
   }
 
   render() {
-    return (
-      <div className="container">
-        <form>
-          <label htmlFor="password"> Password: </label>
-          <div>
-            <input
-              required
-              id="password"
-              type="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
+    const password = this.state.password;
+    if (password === '') {
+      return (
+        <div className="container">
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="password"> Password: </label>
+            <div>
+              <input
+                id="password"
+                type="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+              <i className="fas fa-times x-icon"></i>
+              <p className="error-message">Password is required</p>
+            </div>
+          </form>
+        </div>
+      );
+    } else {
+      return (
+        <div className="container">
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="password"> Password: </label>
+            <div>
+              <input
+                required
+                id="password"
+                type="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+            </div>
+          </form>
+        </div>
+      );
+    }
+
   }
 }
