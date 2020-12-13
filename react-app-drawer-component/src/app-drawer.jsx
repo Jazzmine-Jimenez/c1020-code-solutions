df8602ee5482dd56a4141a6ea59202f90711e17b
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-12-12 16:26:21 -0800

    renders menu bar

---
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Drawer extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="menu-button">
+          <i className="fas fa-bars"></i>
+        </div>
+      </div>
+    );
+  }
+}
