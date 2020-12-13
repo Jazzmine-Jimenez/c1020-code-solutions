8609cf2b7e28a3b9d02da13dd9485c2eef1e98d4
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-12-13 13:45:05 -0800

    functioning handleChange method

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 
 export default class Validate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { password: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ password: event.target.password });
+  }
+
   render() {
     return (
       <div className="container">
         <form>
           <label htmlFor="password"> Password: </label>
           <div>
-            <input id="password" type="password"/>
-            <i className="fas fa-times x-icon"></i>
-            <p className="error-message">Password is required</p>
+            <input
+              required
+              id="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
           </div>
         </form>
       </div>
