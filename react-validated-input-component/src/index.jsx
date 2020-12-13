6a1c0d0f64753d9949b5a8fd67f425b02dbe78fe
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-12-13 13:25:56 -0800

    renders form

--- react-validated-input-component/src/index.jsx
+++ react-validated-input-component/src/index.jsx
@@ -1,6 +1,8 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Validate from './validated-input';
 
-// ReactDOM.render(
-
-// );
+ReactDOM.render(
+  <Validate />,
+  document.querySelector('#root')
+);
--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -1,1 +1,18 @@
-// import React from 'react';
+import React from 'react';
+
+export default class Validate extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <form>
+          <label htmlFor="password"> Password: </label>
+          <div>
+            <input id="password" type="password"/>
+            <i className="fas fa-times x-icon"></i>
+            <p className="error-message">Password is required</p>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
