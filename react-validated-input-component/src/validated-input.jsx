20d185860aab704d414c86dd578cfa30385ddee9
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-12-14 12:09:44 -0800

    methods added to separate virtual dom

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 export default class Validate extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { password: '' };
+    this.state = {
+      password: ''
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -12,7 +14,9 @@ export default class Validate extends React.Component {
   }
 
   handleChange(event) {
-    this.setState({ password: event.target.value });
+    this.setState({
+      password: event.target.value
+    });
   }
 
   handleSubmit(event) {
@@ -62,12 +66,38 @@ export default class Validate extends React.Component {
     return element;
   }
 
+  lenghtMet() {
+    const element = (
+      <div className="container">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="password"> Password: </label>
+          <div>
+            <input
+              id="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+            <i className="fas fa-check check-icon"></i>
+          </div>
+        </form>
+      </div>
+    );
+    return element;
+  }
+
   render() {
     const password = this.state.password;
     if (password === '') {
       const errorElement = this.emptySubmit();
       return errorElement;
     } else if (password.length > 8) {
+      const lengthMet = this.lenghtMet();
+      return lengthMet;
+    } else if (password.length < 8) {
+      const submitError = this.submitError();
+      return submitError;
+    } else {
       return (
         <div className="container">
           <form onSubmit={this.handleSubmit}>
@@ -79,14 +109,12 @@ export default class Validate extends React.Component {
                 value={this.state.password}
                 onChange={this.handleChange}
               />
-              <i className="fas fa-check check-icon"></i>
+              <i className="fas fa-times x-icon"></i>
+              <p className="error-message">Your password is too short.</p>
             </div>
           </form>
         </div>
       );
-    } else {
-      const submitError = this.submitError();
-      return submitError;
     }
   }
 }
