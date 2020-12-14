356223e3b4c0c1ec81de7353ed0a32f18637fa0c
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-12-14 10:12:42 -0800

    customized emptySubmit method

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -7,6 +7,8 @@ export default class Validate extends React.Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.emptySubmit = this.emptySubmit.bind(this);
+    this.submitError = this.submitError.bind(this);
   }
 
   handleChange(event) {
@@ -18,26 +20,53 @@ export default class Validate extends React.Component {
     console.log('Password:', this.state.password);
   }
 
+  emptySubmit() {
+    const element = (
+      <div className="container">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="password"> Password: </label>
+          <div>
+            <input
+              id="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+            <i className="fas fa-times x-icon"></i>
+            <p className="error-message">Password is required</p>
+          </div>
+        </form>
+      </div>
+    );
+    return element;
+  }
+
+  submitError() {
+    const element = (
+      <div className="container">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="password"> Password: </label>
+          <div>
+            <input
+              id="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+            <i className="fas fa-times x-icon"></i>
+            <p className="error-message">Your password is too short.</p>
+          </div>
+        </form>
+      </div>
+    );
+    return element;
+  }
+
   render() {
     const password = this.state.password;
     if (password === '') {
-      return (
-        <div className="container">
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="password"> Password: </label>
-            <div>
-              <input
-                id="password"
-                type="password"
-                value={this.state.password}
-                onChange={this.handleChange}
-              />
-              <i className="fas fa-times x-icon"></i>
-              <p className="error-message">Password is required</p>
-            </div>
-          </form>
-        </div>
-      );
+      const errorElement = this.emptySubmit();
+      return errorElement;
     } else if (password.length > 8) {
       return (
         <div className="container">
@@ -56,23 +85,8 @@ export default class Validate extends React.Component {
         </div>
       );
     } else {
-      return (
-        <div className="container">
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="password"> Password: </label>
-            <div>
-              <input
-                id="password"
-                type="password"
-                value={this.state.password}
-                onChange={this.handleChange}
-              />
-              <i className="fas fa-times x-icon"></i>
-              <p className="error-message">Your password is too short.</p>
-            </div>
-          </form>
-        </div>
-      );
+      const submitError = this.submitError();
+      return submitError;
     }
   }
 }
